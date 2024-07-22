78aa9e0faad8ce2db0b42b30d6e08658765b714f
Aditya <lapratomo24@yahoo.com>
2024-07-22 21:47:22 +0200

    alternative solution

--- main.py
+++ main.py
@@ -14,37 +14,42 @@ logo = '''
 
 from os import system
 
-list_of_bidders = []
+# list_of_bidders = []
 
-def bidders(name, bid):
-    bidders = {}
+# def bidders(name, bid):
+#     bidders = {}
     
-    bidders["name"] = name
-    bidders["bid"] = bid
-    list_of_bidders.append(bidders)
+#     bidders["name"] = name
+#     bidders["bid"] = bid
+#     list_of_bidders.append(bidders)
 
 def highest_bidder(people):
-    winner = people[0]
+    winning_bid = 0
+    winner = ""
     
-    for person in people[1:]:
-        if person["bid"] > winner["bid"]:
+    for person in people: #people as a list
+        bid_amount = people[person]
+        if bid_amount > winning_bid:
+            winning_bid = bid_amount
             winner = person
+    print(f"The item goes to our highest bidder, {winner}! Congratulations!")
 
 print(logo)
 
+bidders = {}
 auction_end = False
 
 while not auction_end:
     name = input("Enter your name: ")
     bid = int(input("Enter your bid amount: $"))
     
-    bidders(name, bid)
+    bidders[name] = bid
 
     last_bid = input("Do I hear another bid? Type 'yes' or 'no' --> ")
     if last_bid == 'yes':
         system("cls")
     else:
         auction_end = True
-        highest_bidder(list_of_bidders)
-        print(f"The item goes to our highest bidder, {name}! Congratulations!")
+        highest_bidder(bidders)
+        
         
